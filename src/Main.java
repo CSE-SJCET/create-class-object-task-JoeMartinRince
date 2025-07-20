7588a7cd2b49a27e30ef1b64829adf76e81795c4
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2025-07-20 19:17:29 +0530

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
-     // TODO: Create a class called Student with name, rollNumber, and grade
+public class Student {
+    String name;
+    int rollNumber;
+    char grade;
 
-    // TODO: Create a method to display student details
+    // Method to display student details
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Grade: " + grade);
+    }
+}
 public class Main {
-      public static void main(String[] args) {
-        // TODO: Create a Student object and call the method to display details
+    public static void main(String[] args) {
+        // TODO: Create Student object
+        Student student = new Student();
+
+        // TODO: Assign sample data
+        student.name = "Alice Johnson";
+        student.rollNumber = 101;
+        student.grade = 'A';
+
+        // TODO: Call displayDetails()
+        student.displayDetails();
     }
 }
